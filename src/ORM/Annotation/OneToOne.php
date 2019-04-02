a62379610a26e1ae51079e401e151ef88ed7aa17
Ken Ngo <ken.ngo@brighte.com.au>
2019-04-02 15:28:18 +1100

    1.6.5

---
+++ src/ORM/Annotation/OneToOne.php
@@ -0,0 +1,28 @@
+<?php
+namespace Salesforce\ORM\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+use Salesforce\ORM\EntityManager;
+use Salesforce\ORM\RelationHandleInterface;
+use Salesforce\ORM\RelationInterface;
+
+/**
+ * @Annotation
+ */
+class OneToOne extends Annotation implements RelationInterface
+{
+    public $name;
+    public $targetClass;
+    public $field;
+    public $targetField;
+    public $lazy = true;
+
+    /**
+     * @param \Salesforce\ORM\EntityManager $entityManager entity manager
+     * @return \Salesforce\ORM\RelationHandleInterface
+     */
+    public function getHandler(EntityManager $entityManager): RelationHandleInterface
+    {
+        return new \Salesforce\ORM\RelationHandles\OneToOne($entityManager);
+    }
+}
---
+++ src/ORM/RelationHandles/OneToOne.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Salesforce\ORM\RelationHandles;
+
+use Salesforce\ORM\Entity;
+use Salesforce\ORM\RelationHandle;
+use Salesforce\ORM\RelationHandleInterface;
+use Salesforce\ORM\RelationInterface;
+
+class OneToOne extends RelationHandle implements RelationHandleInterface
+{
+    /**
+     * @param \Salesforce\ORM\Entity $entity entity
+     * @param \ReflectionProperty $property property
+     * @param \Salesforce\ORM\RelationInterface $annotation relation
+     * @return void
+     * @throws \Salesforce\ORM\Exception\MapperException
+     * @throws \Salesforce\Client\Exception\ResultException
+     * @throws \Exception
+     */
+    public function handle(Entity &$entity, \ReflectionProperty $property, RelationInterface $annotation)
+    {
+        /* @var \Salesforce\ORM\Annotation\OneToOne $annotation */
+        $mapper = $this->entityManager->getMapper();
+        $value = $mapper->getPropertyValueByFieldName($entity, $annotation->field);
+        $collections = $this->entityManager->findBy($annotation->targetClass, ["{$annotation->targetField} = {$value}"]);
+        $mapper->setPropertyValue($entity, $property, $collections[0]);
+    }
+}
